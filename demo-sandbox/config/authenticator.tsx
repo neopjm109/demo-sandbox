48263dc8ed47c7cc2ffb558beaed16006307315e
JoonMin <neopjm109@gmail.com>
2025-01-07 23:48:11 +0900

    fix: jwtAuth

--- demo-sandbox/config/authenticator.tsx
+++ demo-sandbox/config/authenticator.tsx
@@ -1,25 +1,9 @@
 'use client';
 
 import useJwtAuth from "@/hooks/useJwtAuth";
-import useLink from "@/hooks/useLink";
-import useAppStore from "@/stores/store.app";
 
 const Authenticator = ({ children } : { children?: React.ReactNode }) => {
-    const { setLoading } = useAppStore();
-    const { onLink } = useLink();
-    const { status } = useJwtAuth({
-        matcher: ["/"],
-        onAuthSuccess: () => {
-            setLoading(false);
-            console.log("인증 실패");
-        },
-        onAuthFailure: () => {
-            setLoading(false);
-            console.log("인증 실패");
-            onLink("/jwt");
-        }
-    });
-    
+    useJwtAuth();
     return children;
 }
 
