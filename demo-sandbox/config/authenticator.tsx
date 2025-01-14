69104ff4aab6a324362bd97a2e5c9163e6527c32
JoonMin <neopjm109@gmail.com>
2025-01-14 20:27:16 +0900

    fix: 수정

--- demo-sandbox/config/authenticator.tsx
+++ demo-sandbox/config/authenticator.tsx
@@ -34,8 +34,8 @@ const JwtProvider = ({ children }: any) => {
         } = data;
         setAccessToken(accessToken);
         setRefreshToken(refreshToken);
-        cookies.set("x-access-token", accessToken, { expires: new Date(accessExpire), sameSite: 'lax', secure: true });
-        cookies.set("x-refresh-token", refreshToken, { expires: new Date(refreshExpire), sameSite: 'lax', secure: true });
+        cookies.set("x-access-token", accessToken, { expires: new Date(accessExpire * 1000), sameSite: 'lax', secure: true });
+        cookies.set("x-refresh-token", refreshToken, { expires: new Date(refreshExpire * 1000), sameSite: 'lax', secure: true });
         setStatus(AuthStatus.AUTH);
     }
 
